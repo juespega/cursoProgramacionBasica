cc3a7b7d71fb5ecd6160222022bd77f15a524dd9
juespega <juespega666@gmail.com>
2023-01-22 20:23:52 -0500

    juego mokepon - manipulación de la propiedad innerHTML

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -3,17 +3,19 @@ function iniciarJuego() {
   botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador);
 }
 
+//el get.ElementById -> selecciona un objeto del DOM por su id.
 function seleccionarMascotaJugador() {
   let inputHipodoge = document.getElementById("hipodoge");
   let inputCapipepo = document.getElementById("capipepo");
   let inputRatigueya = document.getElementById("ratigueya");
+  let spanMascotaJugador = document.getElementById("mascota-jugador");
 
   if (inputHipodoge.checked) {
-    alert("Seleccionaste a Hipodoge");
+    spanMascotaJugador.innerHTML = "Hipodoge";
   } else if (inputCapipepo.checked) {
-    alert("Seleccionaste a Capipepo");
+    spanMascotaJugador.innerHTML = "Capipepo";
   } else if (inputRatigueya.checked) {
-    alert("Seleccionaste a Ratigueya");
+    spanMascotaJugador.innerHTML = "Ratigueya";
   } else {
     alert("Selecciona una mascota");
   }
